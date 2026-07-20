001ba47fc571d66df0b3749322c82579ddb1fe29
Jitesh <btech10769.23@bitmesra.ac.in>
2026-07-21 03:05:56 +0530

    backedn url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true },
+      '/api': { target: 'https://locora.onrender.com', changeOrigin: true },
     },
   },
 });
